5fcc2754cef69353910ea7248671be0784eda292
Katrin <ashly12323@gmail.com>
2017-03-11 20:32:55 +0200

    Cretating ['brandLink'] as link to the brand's offical sate

--- clothesshop/clothes.php
+++ clothesshop/clothes.php
@@ -1,28 +1,28 @@
 <?php
 
+//Да се добавят стойности за ключовете brandLink. Трябва да са адресите на офицялните сайтове на съответната марка
+//Виж Шанел
 $clothes = array(
-    array('title' => 'Chanel Dress', 'brand' => 'Chanel', 'price' => 1300, 'pic' => 'chanel-dress.jpg'),
-    array('title' => 'Karl Lagerfeld Dress', 'brand' => 'Karl', 'price' => 1200, 'pic' => 'karl-lagerfeld-dress.jpg'),
-    array('title' => 'Moschino', 'brand' => 'MOSCHINO', 'price' => 300, 'pic' => 'moschino-t-shirt.jpeg'),
-    array('title' => 'Dior Dress', 'brand' => 'Dior', 'price' => 2300, 'pic' => 'dior-dress.jpg')
+    array('title' => 'Chanel Dress', 'brand' => 'Chanel', 'price' => 1300, 'pic' => 'chanel-dress.jpg', 'brandLink' => 'http://www.chanel.com'),
+    array('title' => 'Karl Lagerfeld Dress', 'brand' => 'Karl', 'price' => 1200, 'pic' => 'karl-lagerfeld-dress.jpg', 'brandLink' =>'http://www.karl.com/bg' ),
+    array('title' => 'Moschino', 'brand' => 'MOSCHINO', 'price' => 300, 'pic' => 'moschino-t-shirt.jpeg', 'brandLink' => 'https://www.moschino.com/bg' ),
+    array('title' => 'Dior Dress', 'brand' => 'Dior', 'price' => 2300, 'pic' => 'dior-dress.jpg', 'brandLink' => 'http://www.dior.com/home/en_int')
 );
+?>
+ <h1>Our Collection</h1>
 
-$productTitle = $_GET['title'];
-echo "<h1>Our Collection</h1>";
-
-echo "<ul>";
-
-//Да се добави липсващаат част от в променливата линк, така че линка да предава GET параметър, който да укаже
-//на product.php кой продукт желаем да разгледаме
-//променливата $link да се сложи на подходящо място в кода за да се конструира линк
+<ul>
+<?php
 foreach ($clothes as $item) {
     $link = "http://localhost/clothesshop/product.php?title=" . $item['title'];
-    echo "<li>";
-    echo " Title: <a href='" . $link . "'> " .  $item['title'] . "</a>";
-    echo " Brand: " . $item['brand'];
-    echo " Price: " . $item['price'] . "$ ";
-    echo "<img src='" . $item['pic'] . "' height='100' width='100'/>";
-    echo "</li>";
+?>
+    <li>
+        Title: <a href="<?php echo $link; ?>"> <?php echo  $item['title']; ?></a>
+        Brand: <a href="<?php echo $item['brandLink'];?>"><?php echo $item['brand']; ?></a>
+        Price: <?php echo $item['price']; ?>
+        <img src='<?php echo $item['pic'] ?>' height='100' width='100'/>
+    </li>
+<?php
 }
-
-echo "</ul>";
+?>
+</ul>
