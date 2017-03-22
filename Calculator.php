fe3c9ccd1399c0729274f2805598f82295f5d991
Katrin <ashly12323@gmail.com>
2017-03-22 12:22:19 +0200

    CALC3 - IF LESEIF

--- Calculator.php
+++ Calculator.php
@@ -2,24 +2,22 @@
 function Results ($sum) {
     echo "The result is " . $sum ;
 }
+
+function CalculatorAdd ($a, $b) {
+    echo Results($a + $b );
+}
+
+function CalculatorSubtract($a, $b) {
+    echo Results($a - $b);
+}
+
 function CalculatorMultiply ($a, $b) {
     echo Results($a*$b);
 }
 
-
 function CalculatorDivide ($a,$b) {
     echo Results($a/$b);
 
-}
-
-
-function CalculatorAccumulation ($a, $b) {
-    echo Results($a + $b );
-}
-
-
-function CalculatorExtract($a, $b) {
-    echo Results($a - $b);
 }
 
 $action = $_GET['action'];
@@ -27,8 +25,23 @@ $numberA = $_GET ['a'];
 $numberB = $_GET['b'];
 
 
+if ('add'== $_GET['action']) {
+    echo CalculatorAdd($numberA, $numberB);
+}
+
+elseif ('subtract' == $_GET['action']) {
+    echo CalculatorSubtract($numberA, $numberB);
+}
 
 
+elseif ('divide' == $_GET['action']) {
+    echo CalculatorDivide($numberA, $numberB);
+
+}
+
+elseif ('multiply' == $_GET['action']) {
+    echo CalculatorMultiply($numberA, $numberB);
+}
 
 
 
