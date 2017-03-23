a18b284ae3b17becff3e2a5bda8b288f6dd8102a
Katrin <ashly12323@gmail.com>
2017-03-23 13:51:44 +0200

    vardump

---
+++ var_dump ().php
@@ -0,0 +1,10 @@
+<?php
+$one = 'Katrin' ;
+$two = '0889470255';
+$three = array('Katrin', 'Spas', 'Stefan');
+$four = array('fname' => 'Katrin', 'lname' => 'Beleva', 'age' => 25);
+
+var_dump($one);
+var_dump($two);
+var_dump($three);
+var_dump($four);
