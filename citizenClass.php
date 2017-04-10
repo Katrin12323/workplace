f90aecc9d2e693779200f04ca4427ad017458ad4
Katrin <ashly12323@gmail.com>
2017-04-10 13:29:48 +0300

    all the stuff

--- citizenClass.php
+++ citizenClass.php
@@ -36,3 +36,23 @@ class Citizen {
 $citizen1= new Citizen($_POST['firstName'], $_POST['fathersName'], $_POST['lastName'], $_POST['identificationNumber'], $_POST['birthday'], $_POST['homeTown'], $_POST['currentAddress']);
 $citizen1->showCitizenInfo();
 
+/* От тук надоло е кода за връзка с базата данни и записване на данните от обекта в таблизата citizen*/
+
+// Creating connection to database
+$connectionToDatabase = new mysqli('localhost', 'root', 'xaxanepozna', 'workplace');
+
+// Checking if  connection to database is established
+if ($connectionToDatabase->connect_error) {
+    echo "Connection failed: " . $connectionToDatabase->connect_error;
+    exit;
+} else {
+    echo "Connected successfully";
+}
+
+                                    /*това са имената на колоните от таблицата*/
+$insertQuery = "INSERT INTO citizen (firstName, fathersName, lastName, identificationNumber, birthday, homeTown, currentAddress)
+VALUES('$citizen1->firstName', '$citizen1->fathersName', '$citizen1->lastName', '$citizen1->identificationNumber', '$citizen1->birthday', '$citizen1->homeTown', '$citizen1->currentAddress');";
+
+mysqli_query($connectionToDatabase, $insertQuery);
+mysqli_close ($connectionToDatabase);
+
