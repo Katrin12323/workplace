2dccfb2e881130a2f9d31344fffbbf1d76e8bce2
Katrin <ashly12323@gmail.com>
2017-04-13 15:26:05 +0300

    Two clauses in one query

--- BooksQuerySearchBy.php
+++ BooksQuerySearchBy.php
@@ -6,25 +6,17 @@ if ($connection->connect_error) {
     exit;
 }
 $genreInsertByUser = $_POST['genre'];
-$query = "SELECT * FROM Books WHERE genre = '$genreInsertByUser';";
-$genreResults = mysqli_query($connection, $query);
-
-while ($booksGenre = $genreResults->fetch_assoc()) {
-    $genreObject = new Books ($booksGenre['booksName'], $booksGenre['genre'],$booksGenre['author'], $booksGenre ['yearOfIssue'], $booksGenre['Resume'], $booksGenre['whatIsYourOpinion']);
-    $genreObject->showBooksInfo();
-    echo "</br>";
-
-}
-
 $yearOfIssueInsertByUser = $_POST['yearOfIssue'];
-$query = "SELECT * FROM Books WHERE yearOfIssue = '$yearOfIssueInsertByUser';";
-$yearOfIssueResults = mysqli_query($connection, $query);
+$query = "SELECT * FROM Books WHERE genre = '$genreInsertByUser' AND yearOfIssue = '$yearOfIssueInsertByUser';";
+$resultsBySearch = mysqli_query($connection, $query);
 
-while ($bookYear = $yearOfIssueResults->fetch_assoc()) {
-    $yearOfIssueObject = new Books ($bookYear['booksName'], $bookYear['genre'], $bookYear['author'], $bookYear['yearOfIssue'], $bookYear['Resume'], $bookYear['whatIsYourOpinion']);
-    $yearOfIssueObject->showBooksInfo();
+while ($bookResults = $resultsBySearch->fetch_assoc()) {
+    $bookObject = new Books ($bookResults['booksName'], $bookResults['genre'],$bookResults['author'], $bookResults ['yearOfIssue'], $bookResults['Resume'], $bookResults['whatIsYourOpinion']);
+    $bookObject->showBooksInfo();
     echo "</br>";
+
 }
+
 
 
 
