9c44eb3300c08b8e46085dbbceb0d8937e5e66be
Katrin <ashly12323@gmail.com>
2017-04-13 15:00:49 +0300

    WHERE CLAUSE - books 2

--- BooksQuerySearchBy.php
+++ BooksQuerySearchBy.php
@@ -10,8 +10,21 @@ $query = "SELECT * FROM Books WHERE genre = '$genreInsertByUser';";
 $genreResults = mysqli_query($connection, $query);
 
 while ($booksGenre = $genreResults->fetch_assoc()) {
-        $genreObject = new Books ($booksGenre['booksName'], $booksGenre['genre'],$booksGenre['author'], $booksGenre ['yearOfIssue'], $booksGenre['Resume'], $booksGenre['whatIsYourOpinion'] );
-        $genreObject->showBooksInfo();
-        echo "</br>";
+    $genreObject = new Books ($booksGenre['booksName'], $booksGenre['genre'],$booksGenre['author'], $booksGenre ['yearOfIssue'], $booksGenre['Resume'], $booksGenre['whatIsYourOpinion']);
+    $genreObject->showBooksInfo();
+    echo "</br>";
 
 }
+
+$yearOfIssueInsertByUser = $_POST['yearOfIssue'];
+$query = "SELECT * FROM Books WHERE yearOfIssue = '$yearOfIssueInsertByUser';";
+$yearOfIssueResults = mysqli_query($connection, $query);
+
+while ($bookYear = $yearOfIssueResults->fetch_assoc()) {
+    $yearOfIssueObject = new Books ($bookYear['booksName'], $bookYear['genre'], $bookYear['author'], $bookYear['yearOfIssue'], $bookYear['Resume'], $bookYear['whatIsYourOpinion']);
+    $yearOfIssueObject->showBooksInfo();
+    echo "</br>";
+}
+
+
+
