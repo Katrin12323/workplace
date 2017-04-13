68aae40cb2f48e4fe49ff55c04eb04833113e41e
Katrin <ashly12323@gmail.com>
2017-04-13 11:37:01 +0300

    WHERE CLAUSE - newsPaper

--- newspaperClass.php
+++ newspaperClass.php
@@ -2,7 +2,7 @@
 
 require './classes/newspaper.php';
 
-$vouge = new Newspaper($_POST['name'], $_POST['type'], $_POST['dateOfIssue'], $_POST['theme'], $_POST['price']);
+$vouge = new Newspaper($_POST['name'], $_POST['kind'], $_POST['dateOfIssue'], $_POST['theme'], $_POST['price']);
 $vouge->showNewspaperInfo();
 
 // Creating connection to database
@@ -17,8 +17,8 @@ if ($connectionToDatabase->connect_error) {
 }
 
 /*това са имената на колоните от таблицата*/
-$insertQuery = "INSERT INTO newspaper (`name`, `type`, dateOfIssue, theme, price)
-VALUES('$vouge->name', '$vouge->type', '$vouge->dateOfIssue', '$vouge->theme', '$vouge->price');";
+$insertQuery = "INSERT INTO newspaper (`name`, kind, dateOfIssue, theme, price)
+VALUES('$vouge->name', '$vouge->kind', '$vouge->dateOfIssue', '$vouge->theme', '$vouge->price');";
 
 mysqli_query($connectionToDatabase, $insertQuery);
 mysqli_close ($connectionToDatabase);
