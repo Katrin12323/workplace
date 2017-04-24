ef80db2670f380cf962e1715affdedc27bc719cb
Katrin <ashly12323@gmail.com>
2017-04-24 11:02:43 +0300

    Learning extending classes in  OOP

---
+++ oop-stuff/extending-class.php
@@ -0,0 +1,54 @@
+<?php
+
+class Animal
+{
+    protected $name;
+    protected $isPredator;
+    protected $territory;
+
+    public function __construct($_name, $_isPredator, $_territory)
+    {
+        $this->name = $_name;
+        $this->isPredator = $_isPredator;
+        $this->territory = $_territory;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIsPredator()
+    {u
+        return $this->isPredator;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTerritory()
+    {
+        return $this->territory;
+    }
+
+}
+
+class Lion extends Animal
+{
+    public function __construct()
+    {
+        $this->name = "Lion";
+        $this->isPredator = true;
+        $this->territory = "Africa";
+    }
+
+}
+
+$lion = new Lion();
+var_dump($lion->getIsPredator());
